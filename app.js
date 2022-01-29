fc9f27fed7eb5e90c316d7d1cb33cee91a5b9e98
Jeff Ripke <jripke74@gmail.com>
2022-01-29 08:54:09 -0800

    add variables

---
+++ app.js
@@ -0,0 +1,12 @@
+let age = 32;
+let userName = 'Jeff';
+let hobbies = ['Sports', 'Cooking', 'Reading'];
+let job = {
+  title: 'Developer',
+  place: 'New York',
+  salary: 120000,
+};
+
+let adultYears = age - 18;
+alert(hobbies[0]);
+alert(job.place);
